b2958c6f0a25cedebe391d84c5685aa20087a464
Bryan Dady <bryan@subsplash.com>
2023-07-23 18:31:20 -0600

    fix(setup): accommodate MacOS behavior of `sed`

--- docker_setup.sh
+++ docker_setup.sh
@@ -2,7 +2,7 @@
 
 export LC_CTYPE=C
 # :alnum: regex character class includes all Alphanumeric characters
-postgresPassword=$(cat /dev/urandom | tr -cd '[[:alnum:]]' | fold -w 64 | head -n 1)
+POSTGRESPASSWORD=$(cat /dev/urandom | tr -cd '[[:alnum:]]' | fold -w 64 | head -n 1)
 JWTKEY=$(cat /dev/urandom | tr -cd '[[:alnum:]]' | fold -w 256 | head -n 1)
 CRYPTOKEY=$(cat /dev/urandom | tr -cd '[[:alnum:]]' | fold -w 32 | head -n 1)
 
@@ -18,8 +18,14 @@ fi
 
 cp template.docker.env docker.env
 
-sed -i '' "s/JWT_KEY=.*$/JWT_KEY=$JWTKEY/" docker.env
-sed -i '' "s/CRYPTO_KEY=.*$/CRYPTO_KEY=$CRYPTOKEY/" docker.env
-sed -i '' "s/POSTGRES_PASSWORD=.*$/POSTGRES_PASSWORD=$postgresPassword/" docker.env
+# accommodate different behavior by `sed` on MacOS
+SPACER=""
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  SPACER="''"
+fi
+
+sed -i $SPACER "s/JWT_KEY=.*$/JWT_KEY=$JWTKEY/" docker.env
+sed -i $SPACER "s/CRYPTO_KEY=.*$/CRYPTO_KEY=$CRYPTOKEY/" docker.env
+sed -i $SPACER "s/POSTGRES_PASSWORD=.*$/POSTGRES_PASSWORD=$POSTGRESPASSWORD/" docker.env
 
 echo "Cool! Now fill in any missing environment variables in docker.env then run docker-compose up to launch Basedash."
